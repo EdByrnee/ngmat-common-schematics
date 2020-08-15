3c2b5f45133cabe5e48e3626eb508b5ea36972d1
Ed Byrne <ed_b@hotmail.co.uk>
2020-08-15 17:04:33 +0000

    changes

---
+++ src/crudtemplateservice/schema.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Angular Workspace Options Schema
+ * Say hello to someone
+ */
+export interface Schema {
+    /**
+     * The name of the RESTful object
+     */
+    name: string;
+}
---
+++ src/tfg-dialog/index.d.ts
@@ -0,0 +1,3 @@
+import { Rule } from '@angular-devkit/schematics';
+import { Schema } from './schema.d';
+export declare function tfgdialog(_options: Schema): Rule;
