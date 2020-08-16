e0623fa18ec3a7cfc943c6148945f906a228152c
edbyrne <ed_b@hotmail.co.uk>
2020-08-16 22:34:07 +0100

    changes

--- src/tfg-form/index.ts
+++ src/tfg-form/index.ts
@@ -6,12 +6,8 @@ import { SchematicsException } from '@angular-devkit/schematics';
 import { getFormFieldHtml } from './functions/get_form_field_html';
 import { Schema } from './schema';
 import { insertAtGivenPointInterface, GivenPoint, insertAtGivenPoints } from '../_shared/finder/main';
-//import { type } from 'os';
-//import { classify, camelize } from '@angular-devkit/core/src/utils/strings';
-
-
-
-
+// import { type } from 'os';
+// import { classify, camelize } from '@angular-devkit/core/src/utils/strings';
 
 
 
@@ -23,33 +19,18 @@ export function main(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
  
 
-    // LOAD THE HTML
-    let html_path =  _options.path + afterSlash(_options.path) + ".component.html";
-    let html:any = tree.read(html_path);
+    // LOAD THE FILES
+    let original_html:string = loadHtmlAndValidate(tree,_options.path);
+    let original_css:string = loadCssAndValidate(tree,_options.path);
+    let original_ts:string = loadTsAndValidate(tree,_options.path);
 
-    // LOAD THE CSS
-    let css_path = _options.path + afterSlash(_options.path) + ".component.scss";
-    let css:any = tree.read(css_path);
-
-    // LOAD THE TYPESCRIPT
-    let ts_path = _options.path + afterSlash(_options.path) + ".component.ts";
-    let ts:any = tree.read(ts_path);
-
-
-    // VALIDATE ANY ERRORS
-    if (!html) throw new SchematicsException(`file does not exist at ` + html_path);
-    if (!ts) throw new SchematicsException(`file does not exist at ` + ts_path);
-    if (!css) throw new SchematicsException(`file does not exist at ` + css);
-
-    // Break up the properties of each field
+    // PARSE ANYTHING EXTRA FROM THE _options
+    // Fields is used for an array of options e.g. when you are creating an array of form fields 
+    // We obviously can't get an array input using the schematics cli
     let fields:any = getUserOptionsObject(_options.fields);
 
 
-    // SET UP THE VARIABLES
-    let original_html                    = html.toString("utf-8");
-    let original_css                     = css.toString("utf-8");
-    let original_ts                      = ts.toString("utf-8");
-
+    // Let's begin ...
     let html_insersions = makeHtml(_options.form_name, fields);
     let css_insersions = makeCSS();
     let ts_insersions = makeTypescript(_options.form_name, fields);
@@ -61,9 +42,7 @@ export function main(_options: Schema): Rule {
 
 
     // COMMIT THE CHANGES
-    tree.overwrite(html_path, final_html);
-    tree.overwrite(css_path, final_css);
-    tree.overwrite(ts_path, final_ts);
+    commitChanges(tree, _options.path, final_html, final_css, final_ts);
 
     return tree;
 
@@ -195,6 +174,37 @@ function getUserOptionsObject(fields: string){
 
     return obj;
   })
+}
+
+
+function loadHtmlAndValidate(tree:any, path:string){
+  let html_path =  path + afterSlash(path) + ".component.html";
+  let html = tree.read(html_path);
+  if (!html) throw new SchematicsException(`file does not exist at ` + html_path);
+  return html.toString("utf-8");
+}
+
+function loadCssAndValidate(tree:any, path:string){
+  let css_path = path + afterSlash(path) + ".component.scss";
+  let css = tree.read(css_path);
+  if (!css) throw new SchematicsException(`file does not exist at ` + css_path);
+  return css.toString("utf-8");
+}
+
+function loadTsAndValidate(tree:any, path:string){
+  let ts_path = path + afterSlash(path) + ".component.ts";
+  let ts = tree.read(ts_path);
+  if (!ts) throw new SchematicsException(`file does not exist at ` + ts_path);
+  return ts.toString("utf-8");
+}
+
+function commitChanges(tree:any, path: string, final_html:string, final_css:string, final_ts:string, ){
+  let html_path =  path + afterSlash(path) + ".component.html";
+  let css_path = path + afterSlash(path) + ".component.scss";
+  let ts_path = path + afterSlash(path) + ".component.ts";
+  tree.overwrite(html_path, final_html);
+  tree.overwrite(css_path, final_css);
+  tree.overwrite(ts_path, final_ts);
 }
 
 
