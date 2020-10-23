669c7bbd0bd1e1720fc450310459b1a67488db15
Ed Byrne <ed_b@hotmail.co.uk>
2020-10-23 11:46:20 +0100

    changes

--- src/tfg-auth-service/files/src/app/auth.service.ts
+++ src/tfg-auth-service/files/src/app/auth.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { environment } from '@environments/environment';
 
 export interface User{
     id:number;
