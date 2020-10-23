13e752a135f3121ec3117a5509584a3d7d0d7f8c
Ed Byrne <ed_b@hotmail.co.uk>
2020-10-23 09:43:00 +0100

    added imports for http-intercept

--- src/tfg-http-intercept/files/src/app/http-intercept.service.ts
+++ src/tfg-http-intercept/files/src/app/http-intercept.service.ts
@@ -1,39 +1,46 @@
-import { Injectable, Inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import {
-  HttpEvent, 
-  HttpInterceptor, 
-  HttpHandler, 
-  HttpRequest,
-  HttpResponse,
-  HttpErrorResponse
-} from '@angular/common/http'
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { Router } from '@angular/router';
+    HttpInterceptor,
+    HttpRequest,
+    HttpResponse,
+    HttpHandler,
+    HttpEvent,
+    HttpErrorResponse
+} from '@angular/common/http';
 
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpConfigInterceptor implements HttpInterceptor {
-    constructor(private router: Router) { }
+    constructor(public router: Router) { }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const token: string = localStorage.getItem('access_token');
+
+        if (token) {
+            request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
+        }
+
+        if (!request.headers.has('Content-Type')) {
+            request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+        }
+
+        request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+
         return next.handle(request).pipe(
             map((event: HttpEvent<any>) => {
                 if (event instanceof HttpResponse) {
                     console.log('event--->>>', event);
+                    // this.errorDialogService.openDialog(event);
                 }
                 return event;
             }),
             catchError((error: HttpErrorResponse) => {
-                let data = {};
-                data = {
-                    reason: error && error.error && error.error.reason ? error.error.reason : '',
-                    status: error.status
-				};
-				if (error.status === 401){
-					// If the token aint valid!
-					localStorage.setItem("access_token", null);
-					this.router.navigateByUrl("/login");
-				}
+                if(error.status === 401){
+                    // TOKEN NOT VALID CASE
+                    this.router.navigateByUrl("/login");
+                }
                 return throwError(error);
             }));
     }
--- src/tfg-http-intercept/index.ts
+++ src/tfg-http-intercept/index.ts
@@ -5,6 +5,7 @@ import { strings } from "@angular-devkit/core";
 
 
 import { Schema } from './schema.d';
+import { GivenPoint, insertAtGivenPoints } from '../_shared/finder/main';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
@@ -12,6 +13,32 @@ export function main(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     
       const sourceTemplates = url("./files");
+
+      // Create the insersions
+      let NEW_PROVIDER = `{ provide: HTTP_INTERCEPTORS, useClass: HttpConfigInterceptor, multi: true }`
+      let NEW_IMPORT = `import { HttpConfigInterceptor } from './http-intercept.service';`
+      let TS_PATH = "/app/app.module.ts";
+      let ORIGINAL_TS = tree.read(TS_PATH);
+
+
+
+      // Configure the insersions
+      let ts_insersions = [
+        {
+          insert: NEW_PROVIDER,
+          at: GivenPoint.END_OF_IMPORTS
+        },
+        {
+          insert: NEW_IMPORT,
+          at: GivenPoint.END_OF_IMPORTS
+        }
+      ]
+
+
+      // Insert the insersions
+      let final_ts = insertAtGivenPoints(ORIGINAL_TS,ts_insersions)
+
+      tree.overwrite(TS_PATH, final_ts);
       
       const createdTemplates = apply(sourceTemplates,[
         template({
